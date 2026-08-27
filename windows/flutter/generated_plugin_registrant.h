184711141d51096de65a53906d8845d7ccfc123a
Zhou-Yujing114514 <fanbook02@outlook.com>
2026-08-27 12:04:08 +0800

    chore: add windows/flutter/generated_plugin_registrant.h

---
+++ windows/flutter/generated_plugin_registrant.h
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#ifndef GENERATED_PLUGIN_REGISTRANT_
+#define GENERATED_PLUGIN_REGISTRANT_
+
+#include <flutter/plugin_registry.h>
+
+// Registers all plugins with the given registry.
+void fl_register_plugins(FlPluginRegistry* registry);
+
+#endif  // GENERATED_PLUGIN_REGISTRANT_
