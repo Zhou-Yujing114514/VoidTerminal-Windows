337323111cea79e866ecbd2c1507eedccc81548b
Zhou-Yujing114514 <fanbook02@outlook.com>
2026-08-27 12:04:42 +0800

    chore: add windows/runner/utils.h

---
+++ windows/runner/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <string>
+#include <vector>
+
+// Parses the command line arguments into a vector of UTF-8 strings.
+std::vector<std::string> GetCommandLineArguments();
+
+#endif  // UTILS_H_
